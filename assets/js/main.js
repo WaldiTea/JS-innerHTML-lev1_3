a0df5b535d342efcfb2653d8432d3b81eb759c16
SuperCoder <supercoder@MacBook-Air-von-SuperCoder.local>
2022-01-18 14:31:00 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,3 @@
+let gallery = document.getElementById('gallery');
+
+gallery.innerHTML = '<figure><img src="assets/img/vorhang.jpg" alt="Vorhang"><figcaption>Fig.1</figcaption></figure>' + '<figure><img src="assets/img/vorhang.jpg" alt="Vorhang"><figcaption>Fig.2</figcaption></figure>' + '<figure><img src="assets/img/vorhang.jpg" alt="Vorhang"><figcaption>Fig.3</figcaption></figure>';
